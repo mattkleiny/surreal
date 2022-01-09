ee761a1bb216c28a678ff2c0e5496bad091803d7
Matt Kleinschafer <xeusalmighty@gmail.com>
2022-01-09 18:32:46 +1100

    Start adding toolkit for tactical decision trees.

---
+++ toolkits/Surreal.Toolkits.Tactics.Tests/TacticTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Surreal;
+
+public class TacticTests
+{
+  [Test, AutoFixture]
+  public void it_should_evaluate_conditions_and_actions(TacticCondition condition, TacticAction action)
+  {
+    var context = new TacticContext();
+
+    condition.CanExecute(context).Returns(true);
+    action.CanExecute(context).Returns(true);
+
+    var strategy = new Strategy("Prioritize healing")
+    {
+      Tactics =
+      {
+        new Tactic
+        {
+          Conditions = { condition },
+          Actions    = { action },
+        },
+      },
+    };
+
+    strategy.Evaluate(context);
+
+    condition.Received(1).CanExecute(context);
+    action.Received(1).CanExecute(context);
+    action.Received(1).Execute(context);
+  }
+}
---
+++ toolkits/Surreal.Toolkits.Tactics/Tactic.cs
@@ -0,0 +1,70 @@
+﻿namespace Surreal;
+
+/// <summary>Context for <see cref="Tactic"/> operations.</summary>
+public sealed record TacticContext
+{
+  public object? UserData { get; init; }
+}
+
+/// <summary>A set of <see cref="Tactic"/>s that can be evaluated and executed.</summary>
+public sealed record Strategy(string Name)
+{
+  public List<Tactic> Tactics { get; init; } = new();
+
+  public ValueTask Evaluate(TacticContext context)
+  {
+    foreach (var tactic in Tactics)
+    {
+      if (tactic.CanExecute(context))
+      {
+        return tactic.Execute(context);
+      }
+    }
+
+    return ValueTask.CompletedTask;
+  }
+}
+
+/// <summary>A single tactic that can be evaluated for it's objectives.</summary>
+public sealed record Tactic
+{
+  public List<TacticCondition> Conditions { get; init; } = new();
+  public List<TacticAction>    Actions    { get; init; } = new();
+
+  public bool CanExecute(TacticContext context)
+  {
+    foreach (var condition in Conditions)
+    {
+      if (condition.CanExecute(context))
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  public async ValueTask Execute(TacticContext context)
+  {
+    foreach (var action in Actions)
+    {
+      if (action.CanExecute(context))
+      {
+        await action.Execute(context);
+      }
+    }
+  }
+}
+
+/// <summary>Describes a possible condition for a <see cref="Tactic"/>.</summary>
+public abstract record TacticCondition
+{
+  public abstract bool CanExecute(TacticContext context);
+}
+
+/// <summary>Describes a possible action for a <see cref="Tactic"/>.</summary>
+public abstract record TacticAction
+{
+  public abstract bool      CanExecute(TacticContext context);
+  public abstract ValueTask Execute(TacticContext context);
+}
