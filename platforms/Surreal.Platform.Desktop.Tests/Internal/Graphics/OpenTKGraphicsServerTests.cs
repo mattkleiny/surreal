68a9593aa2595b269552acb45f77e4486b4db2df
Matt Kleinschafer <xeusalmighty@gmail.com>
2022-01-23 19:55:40 +1100

    Remove some buggy test code.

--- platforms/Surreal.Platform.Desktop.Tests/Internal/Graphics/OpenTKGraphicsServerTests.cs
+++ platforms/Surreal.Platform.Desktop.Tests/Internal/Graphics/OpenTKGraphicsServerTests.cs
@@ -1,46 +1,10 @@
-﻿using OpenTK.Mathematics;
-using OpenTK.Windowing.Common;
-using OpenTK.Windowing.Desktop;
-using Surreal.Graphics.Shaders;
+﻿using Surreal.Graphics.Shaders;
 using Surreal.IO;
 
 namespace Surreal.Internal.Graphics;
 
 public class OpenTKGraphicsServerTests
 {
-  private GameWindow window = null!;
-
-  [OneTimeSetUp]
-  public void OnSetUp()
-  {
-    var gameWindowSettings = new GameWindowSettings
-    {
-      RenderFrequency = 0,
-      UpdateFrequency = 0,
-      IsMultiThreaded = false,
-    };
-
-    var nativeWindowSettings = new NativeWindowSettings
-    {
-      Title        = "Surreal Tests",
-      Size         = new Vector2i(256, 144),
-      WindowBorder = WindowBorder.Fixed,
-      StartVisible = false,
-    };
-
-    window = new GameWindow(gameWindowSettings, nativeWindowSettings)
-    {
-      VSync = VSyncMode.On,
-    };
-  }
-
-  [OneTimeTearDown]
-  public void OnTearDown()
-  {
-    window.Close();
-    window.Dispose();
-  }
-
   [Test, Ignore("Not yet implemented")]
   [TestCase("Assets/shaders/test01.shade")]
   public async Task it_should_compile_a_shader(VirtualPath path)
