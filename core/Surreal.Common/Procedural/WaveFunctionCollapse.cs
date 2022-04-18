7db434ce6bbeb671e7d74bb7e40a2fd5b72613c6
Matt Kleinschafer <xeusalmighty@gmail.com>
2022-04-18 22:25:24 +1000

    Stub a wave function collapse test.

---
+++ core/Surreal.Common/Procedural/WaveFunctionCollapse.cs
@@ -0,0 +1,23 @@
+﻿using Surreal.Mathematics;
+
+namespace Surreal.Procedural;
+
+/// <summary>A tool for performing wave-function-collapse style generation on a space.</summary>
+/// <remarks>See https://github.com/mxgmn/WaveFunctionCollapse for more information</remarks>
+public sealed class WaveFunctionCollapse<T>
+{
+  public void Tick()
+  {
+    throw new NotImplementedException();
+  }
+
+  public interface IOutputReceiver
+  {
+    void SetOutput(Point2 position, T value);
+  }
+
+  public sealed record Rule(T Value)
+  {
+    public List<T> ValidAdjacency { get; } = new();
+  }
+}
