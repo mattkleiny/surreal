166b82027d8d02910d6f0d2143c0d27ab8a4925b
Matt Kleinschafer <matt.kleinschafer@xero.com>
2022-04-27 14:33:28 +1000

    Add some error handling and logging to LuaScriptServer.

--- core/Surreal.Scripting/LuaScriptServer.cs
+++ core/Surreal.Scripting/LuaScriptServer.cs
@@ -1,18 +1,24 @@
 ﻿using NLua;
+using Surreal.Diagnostics.Logging;
 
 namespace Surreal.Scripting;
 
 /// <summary>A <see cref="IScriptServer"/> implementation for Lua, based on MoonSharp.</summary>
 public sealed class LuaScriptServer : IScriptServer, IDisposable
 {
+  private static readonly ILog Log = LogFactory.GetLog<LuaScriptServer>();
+
   private readonly Dictionary<ScriptHandle, ScriptEntry> scripts = new();
   private int nextScriptId = 0;
 
   public ScriptHandle CreateScript()
   {
     var handle = new ScriptHandle(Interlocked.Increment(ref nextScriptId));
+    var entry = new ScriptEntry();
 
-    scripts[handle] = new ScriptEntry();
+    scripts[handle] = entry;
+
+    entry.Runtime.LoadCLRPackage();
 
     return handle;
   }
@@ -24,8 +30,15 @@ public sealed class LuaScriptServer : IScriptServer, IDisposable
       throw new InvalidOperationException($"Unable to access script for handle {handle}");
     }
 
-    entry.Code = code;
-    entry.Runtime.DoString(code);
+    try
+    {
+      entry.Code = code;
+      entry.Runtime.DoString(code);
+    }
+    catch (Exception exception)
+    {
+      Log.Error(exception, "Failed to compile script code");
+    }
   }
 
   public object? ExecuteScript(ScriptHandle handle)
@@ -35,20 +48,21 @@ public sealed class LuaScriptServer : IScriptServer, IDisposable
       throw new InvalidOperationException($"Unable to access script for handle {handle}");
     }
 
-    return entry.Runtime.DoString(entry.Code);
+    try
+    {
+      return entry.Runtime.DoString(entry.Code);
+    }
+    catch (Exception exception)
+    {
+      Log.Error(exception, $"Failed to execute script {handle}");
+
+      return null;
+    }
   }
 
   public object? ExecuteScriptFunction(ScriptHandle handle, string functionName)
   {
-    if (!scripts.TryGetValue(handle, out var entry))
-    {
-      throw new InvalidOperationException($"Unable to access script for handle {handle}");
-    }
-
-    var function = entry.Runtime.GetFunction(functionName);
-    var result = function.Call(Array.Empty<object>());
-
-    return result?[0];
+    return ExecuteScriptFunction(handle, functionName, Array.Empty<object>());
   }
 
   public object? ExecuteScriptFunction(ScriptHandle handle, string functionName, object[] parameters)
@@ -58,15 +72,24 @@ public sealed class LuaScriptServer : IScriptServer, IDisposable
       throw new InvalidOperationException($"Unable to access script for handle {handle}");
     }
 
-    var function = entry.Runtime.GetFunction(functionName);
-    var result = function.Call(parameters);
-
-    if (result is not { Length: > 0 })
+    try
     {
+      var function = entry.Runtime.GetFunction(functionName);
+      var result = function.Call(parameters);
+
+      if (result is not { Length: > 0 })
+      {
+        return null;
+      }
+
+      return result[0];
+    }
+    catch (Exception exception)
+    {
+      Log.Error(exception, $"Failed to execute script function {functionName} in script {handle}");
+
       return null;
     }
-
-    return result[0];
   }
 
   public void DeleteScript(ScriptHandle handle)
