f9a314d6c585a3a9bf0d8b0744b7de2b23af3eb3
Matt Kleinschafer <xeusalmighty@gmail.com>
2022-05-07 12:50:36 +1000

    Add Alchemist example project.

---
+++ examples/Surreal.Example.Alchemist/Program.cs
@@ -0,0 +1,29 @@
+﻿var platform = new DesktopPlatform
+{
+  Configuration =
+  {
+    Title          = "Alchemist",
+    IsVsyncEnabled = true,
+    ShowFpsInTitle = true,
+    IconPath       = "resx://Alchemist/Resources/icons/alchemist.png"
+  }
+};
+
+Game.Start(platform, game =>
+{
+  // ReSharper disable AccessToDisposedClosure
+  var graphics = game.Services.GetRequiredService<IGraphicsServer>();
+  var keyboard = game.Services.GetRequiredService<IKeyboardDevice>();
+
+  game.ExecuteVariableStep(_ =>
+  {
+    if (keyboard.IsKeyPressed(Key.Escape))
+    {
+      game.Exit();
+    }
+
+    graphics.ClearColorBuffer(Color.Black);
+  });
+
+  return Task.CompletedTask;
+});
