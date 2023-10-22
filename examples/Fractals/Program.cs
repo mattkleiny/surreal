5b28edab3cb5fb330028665c60b819110e860700
Matt Kleinschafer <xeusalmighty@gmail.com>
2023-10-22 17:30:55 +1100

    Get fractal example working

--- examples/Fractals/Program.cs
+++ examples/Fractals/Program.cs
@@ -12,23 +12,76 @@
       IsTransparent = true
     }
   },
-  Host = GameHost.Create(() =>
+  Host = GameHost.Create(async () =>
   {
     var graphics = Game.Services.GetServiceOrThrow<IGraphicsBackend>();
     var keyboard = Game.Services.GetServiceOrThrow<IKeyboardDevice>();
 
     var canvas = new PixelCanvas(graphics, 256, 144);
 
-    return _ =>
+    var palette = await Game.Assets.LoadAssetAsync<ColorPalette>("resx://Fractals/Assets/Embedded/palettes/raspberry.pal");
+    var constant = new Vector2(0.285f, 0.01f);
+
+    return time =>
     {
       graphics.ClearColorBuffer(new Color(0.2f, 0.2f, 0.2f, 0.8f));
 
+      DrawFractal(palette, canvas, constant);
       canvas.DrawQuad();
+
+      constant += new Vector2(0.001f, 0.0f) * time.DeltaTime;
 
       if (keyboard.IsKeyPressed(Key.Escape))
       {
         Game.Exit();
       }
     };
+
+    static void DrawFractal(ColorPalette palette, PixelCanvas canvas, Vector2 constant)
+    {
+      var pixels = canvas.Pixels;
+      var scale = 1f / (pixels.Height / 2f);
+
+      for (var y = 0; y < pixels.Height; y++)
+      for (var x = 0; x < pixels.Width; x++)
+      {
+        var initial = new Vector2(
+          (x - pixels.Width / 2f) * scale,
+          (y - pixels.Height / 2f) * scale
+        );
+
+        var iterations = ComputeIterations(initial, constant);
+        if (iterations == 25)
+        {
+          pixels[x, y] = Color32.Clear;
+        }
+        else
+        {
+          pixels[x, y] = palette[iterations % palette.Count];
+        }
+      }
+    }
+
+    static Vector2 ComputeNext(Vector2 initial, Vector2 constant)
+    {
+      var zr = initial.X * initial.X - initial.Y * initial.Y + constant.X;
+      var zi = 2f * initial.X * initial.Y + constant.Y;
+
+      return new Vector2(zr, zi);
+    }
+
+    static int ComputeIterations(Vector2 initial, Vector2 constant)
+    {
+      var current = initial;
+      var iterations = 0;
+
+      while (current.LengthSquared() < 4 && iterations < 25)
+      {
+        current = ComputeNext(current, constant);
+        iterations++;
+      }
+
+      return iterations;
+    }
   })
 });
