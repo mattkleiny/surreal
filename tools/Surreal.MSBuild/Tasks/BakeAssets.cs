efab2d698299e4972ae738d16a04ec3a1396dbfe
Matt Kleinschafer <matt.kleinschafer@karbonhq.com>
2023-11-02 13:13:12 +1100

    Add tools folder and stub MSBuild project

---
+++ tools/Surreal.MSBuild/Tasks/BakeAssets.cs
@@ -0,0 +1,15 @@
+﻿using Task = Microsoft.Build.Utilities.Task;
+
+namespace Surreal.Tasks;
+
+/// <summary>
+/// An MSBuild task that bakes assets as part of the build pipeline.
+/// </summary>
+[UsedImplicitly]
+public sealed class BakeAssets : Task
+{
+  public override bool Execute()
+  {
+    throw new NotImplementedException();
+  }
+}
